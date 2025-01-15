f280544f1fe589e159f7d299e4aadf6a07d327bb
elen-mou <elen_mou@protonmail.com>
2025-01-15 14:14:37 +0200

    Update hex_numbers.c

--- printf/hex_numbers.c
+++ printf/hex_numbers.c
@@ -1,19 +1,5 @@
+#include <stdio.h>
 
-
-// Special characters' hex codes are of a single digit.
-int print_sp_ch() {
-  char ch = '\r';
-  printf("%x\n", ch);
-  return 0;
-}
-
-// Masking is a technique used to print a hex code without
-// the '0x' at the start.
-int print_with_mask() {
-  char ch = 'a';
-  printf("%x\n", ch & 0xff);
-  return 0;
-}
 
 
 int main(int argc, char **argv) {
